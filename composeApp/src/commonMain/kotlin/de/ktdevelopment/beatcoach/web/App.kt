c8d622ebcea3a71484d7ee92974cce69d641a971
Kevin <kevin.thuermann@web.de>
2026-05-14 20:42:54 +0200

    improve loading of app

--- composeApp/src/commonMain/kotlin/de/ktdevelopment/beatcoach/web/App.kt
+++ composeApp/src/commonMain/kotlin/de/ktdevelopment/beatcoach/web/App.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.width
@@ -23,6 +24,13 @@ import androidx.navigation.navDeepLink
 import beatcoachwebsite.composeapp.generated.resources.Res
 import beatcoachwebsite.composeapp.generated.resources.app_title
 import beatcoachwebsite.composeapp.generated.resources.beatcoach_wortmarke_lila
+import beatcoachwebsite.composeapp.generated.resources.pages__blog__label
+import beatcoachwebsite.composeapp.generated.resources.pages__features__label
+import beatcoachwebsite.composeapp.generated.resources.pages__home__android__button__label
+import beatcoachwebsite.composeapp.generated.resources.pages__home__hero__header
+import beatcoachwebsite.composeapp.generated.resources.pages__home__hero__text
+import beatcoachwebsite.composeapp.generated.resources.pages__home__ios__button__label
+import beatcoachwebsite.composeapp.generated.resources.pages__home__label
 import de.ktdevelopment.beatcoach.web.blog.BlogScreen
 import de.ktdevelopment.beatcoach.web.contact.ContactScreen
 import de.ktdevelopment.beatcoach.web.features.FeaturesScreen
@@ -47,75 +55,75 @@ fun App(
 ) {
     val navController = rememberNavController()
 
-    var isVisible by remember { mutableStateOf(false) }
+    var isVisible by remember { mutableStateOf(true) }
 
-    // Sobald die Composable "ge-launched" wird, starten wir das Einblenden
     LaunchedEffect(Unit) {
-        delay(500.milliseconds)
-        isVisible = true
+        delay(800.milliseconds)
+        isVisible = false
     }
 
     BeatCoachTheme {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colorScheme.background), // keine Ahnung warum, aber sonst flackert es
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            if (!isVisible) {
-                Image(
-                    modifier = Modifier.width(338.dp),
-                    imageVector = vectorResource(Res.drawable.beatcoach_wortmarke_lila),
-                    contentDescription = stringResource(Res.string.app_title),
-                )
-            }
-            AnimatedVisibility(
-                visible = isVisible,
-                enter = fadeIn(animationSpec = tween(durationMillis = 500)),
-                exit = fadeOut()
-            ) {
-                CompositionLocalProvider(LocalNavController provides navController) {
-                    MainScaffold(navController) {
-                        NavHost(
-                            navController = navController,
-                            startDestination = Platform.startRoute
+        Box {
+            CompositionLocalProvider(LocalNavController provides navController) {
+                MainScaffold(navController) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Platform.startRoute
+                    ) {
+                        composable<AppDestination.Home>(
+                            deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#home"))
                         ) {
-                            composable<AppDestination.Home>(
-                                deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#home"))
-                            ) {
-                                HomeScreen()
-                            }
-                            composable<AppDestination.Features>(
-                                deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#features"))
-                            ) {
-                                FeaturesScreen()
-                            }
-                            composable<AppDestination.Blog>(
-                                deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#blog"))
-                            ) {
-                                BlogScreen()
-                            }
-                            composable<AppDestination.Contact>(
-                                deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#contact"))
-                            ) {
-                                ContactScreen()
-                            }
-                            composable<AppDestination.PrivacyPolicy>(
-                                deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#privacy"))
-                            ) {
-                                PrivacyScreen()
-                            }
-                            composable<AppDestination.Impressum>(
-                                deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#impressum"))
-                            ) {
-                                ImpressumScreen()
-                            }
+                            HomeScreen()
                         }
-                        LaunchedEffect(navController) {
-                            onLaunch(navController)
+                        composable<AppDestination.Features>(
+                            deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#features"))
+                        ) {
+                            FeaturesScreen()
+                        }
+                        composable<AppDestination.Blog>(
+                            deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#blog"))
+                        ) {
+                            BlogScreen()
+                        }
+                        composable<AppDestination.Contact>(
+                            deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#contact"))
+                        ) {
+                            ContactScreen()
+                        }
+                        composable<AppDestination.PrivacyPolicy>(
+                            deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#privacy"))
+                        ) {
+                            PrivacyScreen()
+                        }
+                        composable<AppDestination.Impressum>(
+                            deepLinks = listOf(navDeepLink<AppDestination.Home>(basePath = "${Platform.baseUrl}/#impressum"))
+                        ) {
+                            ImpressumScreen()
                         }
                     }
+                    LaunchedEffect(navController) {
+                        onLaunch(navController)
+                    }
+                }
+            }
+
+            AnimatedVisibility(
+                visible = isVisible,
+                enter = fadeIn(),
+                exit = fadeOut(animationSpec = tween(durationMillis = 800)),
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(
+                        modifier = Modifier.width(338.dp),
+                        imageVector = vectorResource(Res.drawable.beatcoach_wortmarke_lila),
+                        contentDescription = stringResource(Res.string.app_title),
+                    )
                 }
             }
         }
